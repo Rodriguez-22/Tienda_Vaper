6087a5cb202bdc4f35356285b67c9e4f7cc31a5e
Rodriguez-22 <jrodrod594@g.educaand.es>
2025-03-05 10:11:59 +0100

    First Commit

---
+++ Tienda/src/main/java/com/jj/tienda_vaper/TiendaVaperApplication.java
@@ -0,0 +1,13 @@
+package com.jj.tienda_vaper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendaVaperApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendaVaperApplication.class, args);
+	}
+
+}
---
+++ Tienda/src/test/java/com/jj/tienda_vaper/TiendaVaperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jj.tienda_vaper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendaVaperApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
