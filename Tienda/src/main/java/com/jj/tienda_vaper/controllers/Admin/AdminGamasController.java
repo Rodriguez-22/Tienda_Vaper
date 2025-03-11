12c038225c13585f9ee32e29a3e2c907ccc1dcef
Rodriguez-22 <jrodrod594@g.educaand.es>
2025-03-11 13:36:40 +0100

    AdminGamas_modificacion

--- Tienda/src/main/java/com/jj/tienda_vaper/controllers/Admin/AdminGamasController.java
+++ Tienda/src/main/java/com/jj/tienda_vaper/controllers/Admin/AdminGamasController.java
@@ -1,5 +1,20 @@
 package com.jj.tienda_vaper.controllers.Admin;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+@Controller
 public class AdminGamasController {
-    
+    @GetMapping("/admin/gamas")
+    public String gamas() {
+        System.out.println("Admin gamas");
+        return "/admin/gamas/index";
+    }
+
+    @GetMapping("/admin/gamas/new")
+    public String newgama() {
+        System.out.println("Admin gamas New");
+        return "/admin/gamas/new";
+    }
 }
